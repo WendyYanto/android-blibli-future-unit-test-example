848af090d7d7498326bc1b48dd5bda7dc315eb11
wendyyanto <yanto.wendy98@gmail.com>
2021-09-17 20:42:25 +0700

    fourth: getUserCase unit test error

--- app/src/test/java/com/dev/unittestexample/domain/GetUserUserCaseTest.kt
+++ app/src/test/java/com/dev/unittestexample/domain/GetUserUserCaseTest.kt
@@ -13,11 +13,19 @@ class GetUserUserCaseTest {
     private val getUserUseCase = GetUserUseCase()
 
     @Test
-    fun `getUser should return user with USER_ID`() {
+    fun `getUser should return user with USER_ID and valid uniqueCode`() {
         val user = getUserUseCase.getUser("USER_ID")
 
         Assert.assertEquals(user?.id, "USER_ID")
         Assert.assertEquals(user?.getUniqueCode(), "User_USER_ID_2021_at_25")
+    }
+
+    @Test
+    fun `getUser should return user with YANTO_USER_ID and valid uniqueCode`() {
+        val user = getUserUseCase.getUser("ID_YANTO")
+
+        Assert.assertEquals(user?.id, "ID_YANTO")
+        Assert.assertEquals(user?.getUniqueCode(), "YANTO_ID_YANTO_2021_at_25")
     }
 
     @Test
