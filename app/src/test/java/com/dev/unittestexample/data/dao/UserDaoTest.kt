64398bc0eab93de4a7441a1a3db2e1b6321ed97c
wendyyanto <yanto.wendy98@gmail.com>
2021-09-25 22:22:08 +0700

    rename unit tests

--- app/src/test/java/com/dev/unittestexample/data/dao/UserDaoTest.kt
+++ app/src/test/java/com/dev/unittestexample/data/dao/UserDaoTest.kt
@@ -13,9 +13,12 @@ class UserDaoTest {
     private val userDao = UserDao()
 
     @Test
-    fun `getUser should return user with id equals to ID`() {
+    fun `getUser should return valid user data`() {
         val result = userDao.getUser("ID")
 
         Assert.assertEquals(result.id, "ID")
+        Assert.assertEquals(result.getUniqueCode(), "User_ID_2021_at_25")
+        Assert.assertEquals(result.age, 25)
+        Assert.assertEquals(result.name, "User")
     }
 }
