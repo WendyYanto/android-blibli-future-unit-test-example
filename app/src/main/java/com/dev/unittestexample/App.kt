74e7b6b516050673b4eba5a3cd98d00bc3386283
wendyyanto <yanto.wendy98@gmail.com>
2021-09-25 21:32:34 +0700

    setup dagger hilt

---
+++ app/src/main/java/com/dev/unittestexample/App.kt
@@ -0,0 +1,14 @@
+package com.dev.unittestexample
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+/**
+ * Created by wendy.yanto on 9/25/2021
+ */
+
+@HiltAndroidApp
+class App: Application() {
+
+}
--- app/src/main/java/com/dev/unittestexample/view/MainActivity.kt
+++ app/src/main/java/com/dev/unittestexample/view/MainActivity.kt
@@ -3,10 +3,13 @@ package com.dev.unittestexample.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dev.unittestexample.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
