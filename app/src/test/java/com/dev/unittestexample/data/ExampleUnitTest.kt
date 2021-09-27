17006e2dc7fa9b41abdac8c001260e5f953691ec
wendyyanto <yanto.wendy98@gmail.com>
2021-09-27 14:19:08 +0700

    add example unit test

---
+++ app/src/test/java/com/dev/unittestexample/data/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.dev.unittestexample.data
+
+import org.junit.Assert
+import org.junit.Test
+
+
+/**
+ * Created by wendy.yanto on 9/27/2021
+ */
+
+class ExampleUnitTest {
+
+    @Test
+    fun test() {
+        Assert.assertEquals("Halo", "Halo")
+    }
+}
